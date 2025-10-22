08d4cf7e6dfbdf3c2f546b5e1f9d2e5a29a24a46
teng122 <donghoangnguyen123@gmail.com>
2025-10-22 18:06:33 +0700

    Converter Update

--- Foodbook.Presentation/Converters/BooleanToVisibilityConverter.cs
+++ Foodbook.Presentation/Converters/BooleanToVisibilityConverter.cs
@@ -50,25 +50,4 @@ namespace Foodbook.Presentation.Converters
             return false;
         }
     }
-
-    public class CountToVisibilityConverter : IValueConverter
-    {
-        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
-        {
-            if (value is int intValue)
-            {
-                return intValue == 0 ? Visibility.Visible : Visibility.Collapsed;
-            }
-            return Visibility.Collapsed;
-        }
-
-        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
-        {
-            if (value is Visibility visibility)
-            {
-                return visibility == Visibility.Visible;
-            }
-            return false;
-        }
-    }
 }
