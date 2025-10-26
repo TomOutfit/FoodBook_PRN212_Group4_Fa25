1c2e67a81532d7ec7f77038fe1ab8a7b7299eef3
Quangson-0502 <155563622+Quangson-0502@users.noreply.github.com>
2025-10-26 19:21:28 +0700

    Update UI - 2nd Edition

--- Foodbook.Presentation/Commands/RelayCommand.cs
+++ Foodbook.Presentation/Commands/RelayCommand.cs
@@ -5,13 +5,27 @@ namespace Foodbook.Presentation.Commands
 {
     public class RelayCommand : ICommand
     {
-        private readonly Func<Task> _execute;
+        private readonly Func<Task>? _executeAsync;
+        private readonly Action? _execute;
         private readonly Func<bool>? _canExecute;
 
         public RelayCommand(Func<Task> execute, Func<bool>? canExecute = null)
         {
+            _executeAsync = execute ?? throw new ArgumentNullException(nameof(execute));
+            _canExecute = canExecute;
+        }
+
+        public RelayCommand(Action execute, Func<bool>? canExecute = null)
+        {
             _execute = execute ?? throw new ArgumentNullException(nameof(execute));
             _canExecute = canExecute;
+        }
+
+        // Overload to support void methods
+        public RelayCommand(Action execute, Action? voidCanExecute = null)
+        {
+            _execute = execute ?? throw new ArgumentNullException(nameof(execute));
+            _canExecute = voidCanExecute != null ? () => { voidCanExecute(); return true; } : null;
         }
 
         public event EventHandler? CanExecuteChanged
@@ -27,7 +41,14 @@ namespace Foodbook.Presentation.Commands
 
         public async void Execute(object? parameter)
         {
-            await _execute();
+            if (_executeAsync != null)
+            {
+                await _executeAsync();
+            }
+            else if (_execute != null)
+            {
+                _execute();
+            }
         }
     }
 
